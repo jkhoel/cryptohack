9eaba6eb4553fb673350fa489b3e685bc6f20d6b
Jens <jens.kristian@kodebyraaet.no>
2021-12-23 17:06:46 +0100

    add: sollution for `Favorite Byte`

--- intro/08_favorite_byte.py
+++ intro/08_favorite_byte.py
@@ -6,18 +6,21 @@ For the next few challenges, you'll use what you've just learned to solve some m
 I've hidden some data using XOR with a single byte, but that byte is a secret. Don't forget to decode from hex first.
 73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d
 """
+from pwn import xor
+from re import search
 
 cipher = bytes.fromhex(
     "73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d")
 
-
-def bxor(b1, b2):
-    result = bytearray(b1)
-    for i, b in enumerate(b2):
-        result[i] ^= b
-    return bytes(result)
-
-
+print("Here is your flag:")
 # A single bytes means an integer value of between 0-255, assuming it is unsigned
-for i in range(0, 255):
-    print(i)
+for i in range(0, 256):
+    # Lets wrap it in a try-catch as some of the XOR results might not be decodable (non-ascii characters)
+    try:
+        flag = xor(cipher, i).decode('utf8')
+
+        # We know that the flag will have a certain format, so we can use that to filter out
+        if search("crypto{", xor(cipher, i).decode('utf8')) is not None:
+            print(flag)
+    except:
+        pass
